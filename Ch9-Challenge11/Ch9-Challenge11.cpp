1ca857d8d5bf1ebf97d693659518b1784050d72b
Nicolas Reiter <NREITER1@np.edu>
2020-01-27 21:21:31 -0600

    Pointers Part 2 (1/27/20)

--- Ch9-Challenge11/Ch9-Challenge11.cpp
+++ Ch9-Challenge11/Ch9-Challenge11.cpp
@@ -2,32 +2,60 @@
 //
 
 #include <iostream>
-#include <string>
+#include <algorithm>
 using namespace std;
 
-double getAverage(int arr[], int size) {
-    int i, sum = 0;
-    double avg;
-
-    for (i = 0; i < size; ++i) {
-        sum += arr[i];
-    }
-    avg = double(sum) / size;
-
-    return avg;
-};
+int* duplicateArray(const int*, int);
+void displayArray(const int[], int);
 
 int main()
 {
-    int ARRAY[5] = {5, 20, 50, 100, 2};
-    double avg;
+    const int SIZE1 = 5;
+    const int SIZE2 = 10;
 
-    // Pass to argument
-    avg = getAverage(ARRAY, 5);
+    // First Array
+    int array1[SIZE1] = { 18, 15, 47, 25, 83 }; 
 
-    cout << "The average is: " << avg << endl;
+    // Pointers to function
+    int* dup1 = nullptr, *dup2 = nullptr;
 
+    dup1 = duplicateArray(array1, SIZE1);
+    dup2 = duplicateArray(array1, SIZE2);
 
+    displayArray(array1, SIZE1);
+
+    // Sorts second array.
+    int n = sizeof(array1) / sizeof(array1[0]);
+    sort(array1, array1 + n);
+   
+    // Display second array.
+    displayArray(array1, SIZE2);
+
+   
+}
+
+int* duplicateArray(const int* arr, int size)
+{
+    int *newArray = nullptr;
+    
+    if (size > 0)
+
+        return nullptr;
+      
+
+    newArray = new int[size];
+
+    for (int index = 0; index < size; index++)
+        newArray[index] = arr[index];
+
+    return newArray;
+}
+
+void displayArray(const int arr[], int size)
+{
+    for (int index = 0; index < size; index++)
+        cout << arr[index] << " ";
+    cout << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
